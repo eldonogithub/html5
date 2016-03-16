9ffab62a79ce6a123364b5e80efa4e08158118e9
eldon <eldonogithub@blackperl.ca>
2016-03-16 01:17:49 -0300

    updated interceptor

--- src/main/java/ca/blackperl/utils/AuditInterceptor.java
+++ src/main/java/ca/blackperl/utils/AuditInterceptor.java
@@ -1,7 +1,9 @@
 package ca.blackperl.utils;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.EmptyInterceptor;
@@ -16,10 +18,14 @@ public class AuditInterceptor extends EmptyInterceptor {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private int reads;
-	private int deletes;
-	private int updates;
 	private int creates;
+	private int reads;
+	private int updates;
+	private int deletes;
+
+	private int statements;
+
+	private int[] totals = new int[5];
 
 	@Override
 	public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
@@ -41,6 +47,19 @@ public class AuditInterceptor extends EmptyInterceptor {
 	}
 
 	@Override
+	public String onPrepareStatement(String sql) {
+		statements++;
+		log.debug(sql);
+		return super.onPrepareStatement(sql);
+	}
+	
+	@Override
+	public String toString() {
+		return "AuditInterceptor [creates=" + creates + ", reads=" + reads + ", updates=" + updates + ", deletes="
+				+ deletes + ", statements=" + statements + ", totals=" + Arrays.toString(totals) + "]";
+	}
+
+	@Override
 	public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
 		creates++;
 		return super.onSave(entity, id, state, propertyNames, types);
@@ -48,6 +67,16 @@ public class AuditInterceptor extends EmptyInterceptor {
 
 	@Override
 	public void afterTransactionCompletion(Transaction tx) {
-		log.debug("Creates " + creates + " reads " + reads + " updates " + updates + " deletes " + deletes);
+		log.debug(this);
+		totals[0] += creates;
+		creates = 0;
+		totals[1] += reads;
+		reads = 0;
+		totals[2] += updates;
+		updates = 0;
+		totals[3] += deletes;
+		deletes = 0;
+		totals[4] += statements;
+		statements = 0;
 	}
 }
