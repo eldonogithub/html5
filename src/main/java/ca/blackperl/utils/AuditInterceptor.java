850671cee79157df1d175d84c3156eb5416374f6
eldon <eldono@namaka.ca>
2016-03-17 17:04:12 -0300

    removed totals

--- src/main/java/ca/blackperl/utils/AuditInterceptor.java
+++ src/main/java/ca/blackperl/utils/AuditInterceptor.java
@@ -1,9 +1,7 @@
 package ca.blackperl.utils;
 
 import java.io.Serializable;
-import java.util.Arrays;
 
-import org.apache.commons.beanutils.BeanUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.hibernate.EmptyInterceptor;
@@ -22,10 +20,7 @@ public class AuditInterceptor extends EmptyInterceptor {
 	private int reads;
 	private int updates;
 	private int deletes;
-
 	private int statements;
-
-	private int[] totals = new int[5];
 
 	@Override
 	public boolean onLoad(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
@@ -52,11 +47,11 @@ public class AuditInterceptor extends EmptyInterceptor {
 		log.debug(sql);
 		return super.onPrepareStatement(sql);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "AuditInterceptor [creates=" + creates + ", reads=" + reads + ", updates=" + updates + ", deletes="
-				+ deletes + ", statements=" + statements + ", totals=" + Arrays.toString(totals) + "]";
+				+ deletes + ", statements=" + statements + "]";
 	}
 
 	@Override
@@ -68,15 +63,5 @@ public class AuditInterceptor extends EmptyInterceptor {
 	@Override
 	public void afterTransactionCompletion(Transaction tx) {
 		log.debug(this);
-		totals[0] += creates;
-		creates = 0;
-		totals[1] += reads;
-		reads = 0;
-		totals[2] += updates;
-		updates = 0;
-		totals[3] += deletes;
-		deletes = 0;
-		totals[4] += statements;
-		statements = 0;
 	}
 }
